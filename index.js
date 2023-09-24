e462b7ad3db2d0b5a4c6a972183a187b2e1d14b8
Peter-Immanuel <peterbemshima@gmail.com>
2023-09-24 08:53:36 +0100

    Redirect Screen for google authentication on desktop

---
+++ index.js
@@ -0,0 +1,22 @@
+function findUrlParameter(parameterName) {
+    let result = null;
+    // clean link of all #
+    var cleanedLink = location.href.replace("#","&")
+    const searchParams = new URLSearchParams(cleanedLink);
+    if (searchParams.has(parameterName)) {
+      result = searchParams.get(parameterName);
+    }
+    return result;
+  }
+  
+function redirectToDesktop() {
+    const idToken = findUrlParameter("id_token");
+    const accessToken = findUrlParameter("access_token");
+    const appLinkScheme = "emailsenderscheme";
+    const appLinkAuthority = "app.emailsender.com";
+    const appLinkUrl = `${appLinkScheme}://${appLinkAuthority}/google-auth?id_token=${idToken}&access_token=${accessToken}`;
+    console.log(appLinkUrl)
+    setTimeout(() => {
+        window.location.href = appLinkUrl;
+    }, 100);
+}
